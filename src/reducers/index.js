8afb13ae2a00068e86438ef21795d260f5056b22
abritylove <48786032+abritylove@users.noreply.github.com>
2019-06-12 15:31:51 +0300

    Create index.js

---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import{combinereducers}from "redux";
+import postsReducer from"./postsReducer";
+import usersReducer from"./usersReducer";
+
+export default combineReducers({posts:postsReducer,users:usersReducer});
