e3cdf9f93142f33ea074f13478bc19c4783b0f43
abritylove <48786032+abritylove@users.noreply.github.com>
2019-06-12 15:28:27 +0300

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Postlist from"./PostList";
+import UserHeader from "./UserHeader";
+const App=()=>{
+return(
+<div className="ui container">
+<postList />
+<UserHeader />
+</div>
+);
+};
+export default App;
